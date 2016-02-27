2b9ed86d79d53d83153f22f690e3d90ecdfd1da6
Keith Yong <terda12@gmail.com>
2016-02-27 10:53:24 -0500

    FormValue text grabbing

--- src/http.go
+++ src/http.go
@@ -17,18 +17,13 @@ func Index(ctx *fasthttp.RequestCtx, _ fasthttprouter.Params) {
     tmpl.Execute(ctx, data)
 }
 
-func Hello(ctx *fasthttp.RequestCtx, ps fasthttprouter.Params) {
-    fmt.Fprintf(ctx, "hello, %s!\n", ps.ByName("name"))
-}
-
 func Save(ctx *fasthttp.RequestCtx, ps fasthttprouter.Params) {
-    fmt.Println(ctx.FormValue("paste-area"))
+    fmt.Println(string(ctx.FormValue("pastearea")))
 }
 
 func main() {
     router := fasthttprouter.New()
     router.GET("/", Index)
-    router.GET("/hello/:name", Hello)
     router.POST("/save", Save)
 
     log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
